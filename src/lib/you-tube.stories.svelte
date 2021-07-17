4e3e1fcffcc2a60ea3cbb0aa947318567bb1bd27
spences10 <spences10apps@gmail.com>
2021-07-17 18:47:42 +0100

    feat: :sparkles: add YouTube story

---
+++ src/lib/you-tube.stories.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import YouTube from './you-tube.svelte'
+</script>
+
+<Meta
+  title="YouTube"
+  component={YouTube}
+  argTypes={{
+    width: { control: 'number' },
+    height: { control: 'number' },
+    youTubeId: { control: 'text' },
+    autoPlay: { control: 'boolean' },
+    aspectRatio: { control: 'text' },
+    skipTo: { control: 'object' },
+  }}
+/>
+
+<Template let:args>
+  <YouTube {...args} />
+</Template>
+
+<Story
+  name="Aspect Ratio"
+  args={{
+    youTubeId: 'mM5_T-F1Yn4',
+  }}
+/>
+
+<Story
+  name="Skip To"
+  args={{
+    youTubeId: 'PS2784YfPpw',
+    skipTo: { h: 0, m: 38, s: 44 },
+  }}
+/>
+
+<Story
+  name="Auto Play"
+  args={{
+    youTubeId: 'mM5_T-F1Yn4',
+    autoPlay: true,
+  }}
+/>
--- src/lib/you-tube.svelte
+++ src/lib/you-tube.svelte
@@ -1,12 +1,37 @@
-<script>
+<script lang="ts">
   export let width = 560
   export let height = 315
-  export let youTubeId
-  export let autoPlay
-  export let startTime
+  export let youTubeId = ''
+  export let autoPlay = false
+  export let aspectRatio = '16:9'
+  export let skipTo = { h: 0, m: 0, s: 0 }
+
+  const { h, m, s } = skipTo 
+
+  const tH = h * 60
+  const tM = m * 60
+
+  const startTime = tH + tM + s
+
+  export const getPadding = aspectRatio => {
+    const config = {
+      '1:1': `padding-top: 100%;`,
+      '16:9': `padding-top: 56.25%;`,
+      '4:3': `padding-top: 75%;`,
+      '3:2': `padding-top: 66.66%;`,
+      '8.5': `padding-top: 62.5%;`,
+    }
+    return config[aspectRatio]
+  }
 </script>
 
-<div style={`position: relative; width: 100%;padding-top: 56.25%;`}>
+<div
+  style={`
+    position: relative;
+    width: 100%;
+    ${getPadding(aspectRatio)}
+  `}
+>
   <iframe
     {width}
     {height}
