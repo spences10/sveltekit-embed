bbc3b89bcd6e02911b03a21fe977d7667de65f8b
spences10 <spences10apps@gmail.com>
2021-07-21 17:52:38 +0100

    feat: :sparkles: add Spotify component w/ story

---
+++ src/lib/spotify.stories.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Spotify from './spotify.svelte'
+</script>
+
+<Meta
+  title="Spotify"
+  component={Spotify}
+  argTypes={{
+    spotifyLink: { control: 'string' },
+    width: { control: 'string' },
+    height: { control: 'string' },
+  }}
+/>
+
+<Template let:args>
+  <Spotify {...args} />
+</Template>
+
+<Story
+  name="Width"
+  args={{
+    spotifyLink: 'album/65KwtzkJXw7oT819NFWmEP',
+    width: '100%',
+  }}
+/>
+
+<Story
+  name="Height"
+  args={{
+    spotifyLink: 'album/65KwtzkJXw7oT819NFWmEP',
+    height: '85px',
+  }}
+/>
---
+++ src/lib/spotify.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let spotifyLink = ''
+  export let width = 320
+  export let height = 380
+</script>
+
+<iframe
+  data-testid="spotify"
+  title={`spotify-${spotifyLink}`}
+  class="spotify-sveltekit-embed"
+  src={`https://open.spotify.com/embed/${spotifyLink}`}
+  {width}
+  {height}
+  frameBorder="0"
+  allow="encrypted-media"
+/>
