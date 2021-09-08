22e6f2c4684a1d56dd1359016310c2bc09e86ea1
spences10 <spences10apps@gmail.com>
2021-09-08 21:03:02 +0100

    feat: :sparkles: add Deezer widget

---
+++ src/lib/deezer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let theme = 'auto'
+  export let frameSrc = ''
+  export let height = '300'
+  export let width = '100%'
+</script>
+
+<iframe
+  title="deezer-widget"
+  src={`https://widget.deezer.com/widget/${theme}/${frameSrc}`}
+  {width}
+  {height}
+  frameborder="0"
+  allowtransparency="true"
+  allow="encrypted-media; clipboard-write"
+/>
