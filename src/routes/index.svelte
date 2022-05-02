48306d5f98e0a7bbef831358266966b096d5603e
spences10 <spences10apps@gmail.com>
2022-05-02 19:11:09 +0100

    refactor: :lipstick: add basic styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,7 @@
     Vimeo,
     YouTube,
   } from 'sveltekit-embed'
+  import '../global.css'
 </script>
 
 <main>
@@ -19,87 +20,122 @@
     packaged up for use.
   </p>
 
-  <h2>AnchorFm</h2>
+  <p>
+    Each component is wrapped in an intersection observer <code>
+      GeneralObserver
+    </code> which will load up the component when it scrolls into the viewport.
+  </p>
+
+  <h3>AnchorFm</h3>
   <p>Props:</p>
   <pre>
-  episodeUrl: string
-  height: string = '100'
-  width: string = '100'
+    episodeUrl: string
+    height: string = '100'
+    width: string = '100'
   </pre>
+
   <AnchorFm
-    height="200"
+    height="165"
     episodeUrl="purrfect-dev/embed/episodes/1-31---Delivering-Digital-Content-with-GraphCMS-e14g55c/a-a650v9a"
   />
 
-  <h2>Buzzsprout</h2>
+  <h3>Buzzsprout</h3>
   <p>Props:</p>
   <pre>
-  buzzsproutId: string
-  width: string = '100'
-  height: string = '200'
+    buzzsproutId: string
+    width: string = '100'
+    height: string = '200'
   </pre>
+
   <Buzzsprout
     buzzsproutId="190346/9866589-185-scott-spence-from-vba-analyst-to-webdev"
   />
 
-  <h2>Deezer</h2>
+  <h3>Deezer</h3>
   <p>Props:</p>
   <pre>
-  theme: string = 'auto'
-  frameSrc: string = ''
-  height: string = '300px'
-  width: string = '100%'
+    theme: string = 'auto'
+    frameSrc: string = ''
+    height: string = '300px'
+    width: string = '100%'
   </pre>
+
   <Deezer frameSrc="show/496882" />
 
-  <h2>SimpleCast</h2>
+  <h3>SimpleCast</h3>
   <p>Props:</p>
   <pre>
-  episodeId: string = ''
-  theme: string = `dark`
+    episodeId: string = ''
+    theme: string = `dark`
   </pre>
+
   <SimpleCast episodeId="46aaf483-8567-451a-aa7c-4c92fbb13f28" />
 
-  <h2>Spotify</h2>
+  <h3>Spotify</h3>
   <p>Props:</p>
   <pre>
-  spotifyLink: string = ''
-  width: string = '320'
-  height: string = '380'
+    spotifyLink: string = ''
+    width: string = '320'
+    height: string = '380'
   </pre>
+
   <Spotify
     spotifyLink="show/4XPl3uEEL9hvqMkoZrzbx5"
     width="100%"
     height="180px"
   />
 
-  <h2>Tweet</h2>
+  <h3>Tweet</h3>
   <p>Props:</p>
   <pre>
-  tweetLink: string = ''
+    tweetLink: string = ''
   </pre>
+
   <Tweet tweetLink="adamwathan/status/959078631434731521" />
 
-  <h2>Vimeo</h2>
+  <h3>Vimeo</h3>
   <p>Props:</p>
   <pre>
-  vimeoId: string = ''
-  autoPlay: boolean = false
-  aspectRatio: string = '16:9'
-  skipTo =  &#123; h: 0, m: 0, s: 0 &#125;
+    vimeoId: string = ''
+    autoPlay: boolean = false
+    aspectRatio: string = '16:9'
+    skipTo =  &#123; h: 0, m: 0, s: 0 &#125;
   </pre>
+
   <Vimeo vimeoId="246846978" />
 
-  <h2>YouTube</h2>
+  <h3>YouTube</h3>
   <p>Props:</p>
   <pre>
-  width: string = '560'
-  height: string = '315'
-  youTubeId: string = ''
-  listId: string = ''
-  autoPlay: boolean = false
-  aspectRatio: string = '16:9'
-  skipTo =  &#123; h: 0, m: 0, s: 0 &#125;
+    width: string = '560'
+    height: string = '315'
+    youTubeId: string = ''
+    listId: string = ''
+    autoPlay: boolean = false
+    aspectRatio: string = '16:9'
+    skipTo =  &#123; h: 0, m: 0, s: 0 &#125;
   </pre>
+
   <YouTube youTubeId="L7_z8rcbFPg" />
 </main>
+
+<style>
+  main {
+    max-width: 800px;
+    margin: 0 auto;
+    margin-bottom: 5em;
+  }
+  pre {
+    padding-top: 1.6em;
+    font-family: 'Courier New', Courier, monospace;
+    background-color: rgb(51, 18, 73);
+    border-radius: 8px;
+    margin-bottom: 1rem;
+  }
+  code {
+    font-family: 'Courier New', Courier, monospace;
+    background-color: rgb(51, 18, 73);
+    border-radius: 8px;
+    padding: 0.2em 0.4em;
+  }
+</style>
