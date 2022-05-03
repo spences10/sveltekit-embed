8ea1d1f1a0bb2a4c7a131943e3cad686bdf4d9e3
spences10 <spences10apps@gmail.com>
2022-05-03 15:23:04 +0100

    refactor: :recycle: rename component for casing

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,8 +3,9 @@ export { default as Buzzsprout } from './components/buzzsprout.svelte'
 export { default as Deezer } from './components/deezer.svelte'
 export { default as GeneralObserver } from './components/general-observer.svelte'
 export { default as SimpleCast } from './components/simple-cast.svelte'
+export { default as SoundCloud } from "./components/sound-cloud.svelte"
 export { default as Spotify } from './components/spotify.svelte'
 export { default as Tweet } from './components/tweet.svelte'
 export { default as Vimeo } from './components/vimeo.svelte'
 export { default as YouTube } from './components/you-tube.svelte'
-export { default as SoundCloud } from "./components/soundcloud.svelte"
+
