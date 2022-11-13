923e9377adf0c81767524860de2b3cc51a136c22
spences10 <spences10apps@gmail.com>
2022-11-13 12:48:01 +0000

    feat: :sparkles: add slides component

---
+++ src/lib/components/slides.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import GeneralObserver from './general-observer.svelte'
+
+  export let width: string = '576'
+  export let height: string = '420'
+  export let username: string = ''
+  export let title: string = ''
+  export let byline: 'hidden' | 'visible' | 'default' = 'hidden'
+  export let share: 'hidden' | 'visible' | 'default' = 'hidden'
+  export let style:
+    | 'light'
+    | 'dark'
+    | 'hidden'
+    | 'transparent'
+    | 'default' = 'dark'
+
+  let baseUrl = `https://slides.com/${username}/${title}/embed?`
+  const config = {
+    style,
+    byline,
+    share,
+  }
+
+  const queryString = new URLSearchParams(config)
+  const src = `${baseUrl}&${queryString.toString()}`
+</script>
+
+<GeneralObserver {height} {width}>
+  <iframe
+    {src}
+    {width}
+    {height}
+    {title}
+    scrolling="no"
+    frameborder="0"
+    allowfullscreen
+    style="width: 100%;"
+  />
+</GeneralObserver>
