dae09b6826561aa18fa7189833d5750144251339
spences10 <spences10apps@gmail.com>
2022-12-18 09:56:01 +0000

    refactor: :label: add types

--- src/lib/utils/use-viewport-action.ts
+++ src/lib/utils/use-viewport-action.ts
@@ -1,20 +1,25 @@
 // Thanks Lihau https://www.youtube.com/watch?v=1SKKzdHVvcI&t=182s
 
-let intersectionObserver
+let intersectionObserver: IntersectionObserver
 
 function ensureIntersectionObserver() {
   if (intersectionObserver) return
 
-  intersectionObserver = new IntersectionObserver(entries => {
-    entries.filter(({isIntersecting}) => isIntersecting).forEach(entry => {
-        entry.target.dispatchEvent(new CustomEvent('enterViewport'))
-    })
-  }, {
-    rootMargin: "1000px"
-  })
+  intersectionObserver = new IntersectionObserver(
+    entries => {
+      entries
+        .filter(({ isIntersecting }) => isIntersecting)
+        .forEach(entry => {
+          entry.target.dispatchEvent(new CustomEvent('enterViewport'))
+        })
+    },
+    {
+      rootMargin: '1000px',
+    }
+  )
 }
 
-export default function viewport(element) {
+export default function viewport(element: Element) {
   ensureIntersectionObserver()
 
   intersectionObserver.observe(element)
