25bd407982cc23e0219fe2973f83d3ffdeb45ae5
João Palmeiro <joaommpalmeiro@gmail.com>
2023-01-12 00:07:11 +0000

    Try aspect-ratio

--- src/lib/components/relive.svelte
+++ src/lib/components/relive.svelte
@@ -2,6 +2,7 @@
   import GeneralObserver from './general-observer.svelte'
 
   export let reliveId: string
+  export let width: string = '100%'
 </script>
 
 <GeneralObserver>
@@ -10,3 +11,12 @@
     src={`https://www.relive.cc/view/${reliveId}/widget`}
   />
 </GeneralObserver>
+
+<style>
+  iframe {
+    /* 2 from margin + 1 from box-shadow */
+    aspect-ratio: 1000 / calc(782.5 + 3);
+    overflow: clip;
+    width: 100%;
+  }
+</style>
