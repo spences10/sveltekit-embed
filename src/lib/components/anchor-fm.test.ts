2a7b479dfcc4d37881c06031d78397f401d0ad13
spences10 <spences10apps@gmail.com>
2023-03-09 06:30:43 +0000

    test: :white_check_mark: add aditional tests

--- src/lib/components/anchor-fm.test.ts
+++ src/lib/components/anchor-fm.test.ts
@@ -25,4 +25,26 @@ describe('AnchorFm', () => {
 		const expectedSrc = `https://anchor.fm/${episodeUrl}`
 		expect(iframe.getAttribute('src')).toBe(expectedSrc)
 	})
+
+	it('mounts with custom height and width', async () => {
+		const { container } = render(AnchorFm, {
+			episodeUrl,
+			height: '200px',
+			width: '50%',
+			disable_observer: true,
+		})
+		const iframe = container.querySelector('iframe')
+
+		expect(iframe.parentElement.style.height).toBe('200px')
+		expect(iframe.parentElement.style.width).toBe('50%')
+	})
+
+	it('renders with a GeneralObserver', async () => {
+		const { getByTestId } = render(AnchorFm, {
+			episodeUrl,
+			disable_observer: false,
+		})
+		const general_observer = getByTestId('general-observer')
+		expect(general_observer).toBeTruthy()
+	})
 })
