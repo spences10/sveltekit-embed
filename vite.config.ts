150a0be6053120f22820379f3ad7909c1c05ba0a
spences10 <spences10apps@gmail.com>
2023-03-12 15:15:50 +0000

    refactor: :recycle: move to use svelte-package v2 scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
 
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 	},
 	// https://vitejs.dev/config/#server-fs-allow
@@ -15,6 +14,4 @@ const config = {
 			allow: ['..'],
 		},
 	},
-}
-
-export default config
+})
