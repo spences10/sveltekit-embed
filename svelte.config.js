b69321cf002b095e44cf5241481e6980bcd748f3
spences10 <spences10apps@gmail.com>
2023-06-29 17:02:58 +0100

    refactor: :recycle: use only vite proprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { mdsvex } from 'mdsvex'
-import preprocess from 'svelte-preprocess'
 import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,13 +9,7 @@ const config = {
 
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		mdsvex(mdsvexConfig),
-		vitePreprocess(),
-		preprocess({
-			postcss: true,
-		}),
-	],
+	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()],
 
 	kit: {
 		adapter: adapter(),
