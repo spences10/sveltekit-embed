a905f62bb680eec41837377877d3c6dae779579d
spences10 <spences10apps@gmail.com>
2024-05-29 20:22:16 +0100

    refactor: :recycle: just pass platform

--- apps/web/src/routes/+layout.server.ts
+++ apps/web/src/routes/+layout.server.ts
@@ -1,11 +1,6 @@
 export const load = ({ platform }) => {
-	const isVercel = platform?.env?.VERCEL === '1';
-
-  console.log('=====================')
-  console.log(platform)
-  console.log('=====================')
 
 	return {
-		isVercel,
+		platform,
 	};
 };
