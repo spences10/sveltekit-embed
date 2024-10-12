d93f096dda8ea450ac910aeda57b4465cf68d314
spences10 <spences10apps@gmail.com>
2024-10-12 18:56:06 +0100

    refactor: :recycle: fix vitest config

--- packages/sveltekit-embed/vite.config.ts
+++ packages/sveltekit-embed/vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteTesting()],
 	test: {
 		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,ts}'],
